aa0fb6323020f63b9111184c0b84f48ba6df233f
SoyouCrypto <soyou.crypto@gmail.com>
2022-11-04 01:03:25 +0900

    .

--- src/components/sections/StableOnly.js
+++ src/components/sections/StableOnly.js
@@ -39,7 +39,7 @@ const StableOnly = forwardRef((props, ref) => {
     }
 
     useEffect(() => {
-      dispatch(stableAction.getStableGraph("STABLEALL"));
+      dispatch(stableAction.getStableGraph("STABLE3M"));
     }, []);
 
     // useEffect(() => {
