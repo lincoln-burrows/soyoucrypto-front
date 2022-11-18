15ccac6fa81abfc5989de14037a14cf5abbb0720
SoyouCrypto <soyou.crypto@gmail.com>
2022-11-18 21:50:29 +0900

    fix error

--- src/components/sections/MomentumGraph.js
+++ src/components/sections/MomentumGraph.js
@@ -80,9 +80,13 @@ class MomentumGraph extends Component {
           label: {
             formatter: function (params) {
               // console.log("요기가 에러야 momentum", params.seriesData.length)
-              return (
-                (params.seriesData && params.seriesData.length ? params.seriesData[0].data[0].toISOString().split("T")[0] :  Math.round(params.value*10000)/100 +' %')
+              // return (
+              //   (params.seriesData && params.seriesData.length ? params.seriesData[0].data[0].toISOString().split("T")[0] :  Math.round(params.value*10000)/100 +' %')
                 
+              // )
+              return (
+                (  params.seriesData && params.seriesData.length > 0
+                  ? params.seriesData[0]?.data[0]?.toISOString().split("T")[0] :  Math.round(params.value*10000)/100 +' %')
               )
             }
             }
